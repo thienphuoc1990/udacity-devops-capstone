7491b423c13fb29e6967749f3be09c2c0e9ac0c4
Phuoc Dinh Thien <dinhthienphuoc1990@gmail.com>
2023-05-27 18:30:06 +0700

    cicd-pipeline: fix failed smoke test

--- scripts/smoke-test.sh
+++ scripts/smoke-test.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-http_code=$(curl -LI '127.0.0.1:8080' -o /dev/null -w '%{http_code}\n' -s)
+echo "Forwarding port to 8080 for run smoke test, wait 10 seconds for port forward"
+kubectl port-forward service/uda-app-service 8080:80 > uda-app-forward-port.log 2>&1 &
+sleep 10
+http_code=$(curl -LI 127.0.0.1:8080 -o /dev/null -w '%{http_code}\n' -s)
 echo $http_code
 if [[ ${http_code} -eq 200  ]]; then
     echo "Smoke test passed"
+    cat uda-app-forward-port.log
     exit 0
 else 
     echo "Smoke test failed"
+    cat uda-app-forward-port.log
     exit 1
 fi
