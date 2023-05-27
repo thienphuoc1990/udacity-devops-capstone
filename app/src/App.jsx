a5b2223a7d599af0dda3fe8574b9acdad8d4c4ed
Phuoc Dinh Thien <dinhthienphuoc1990@gmail.com>
2023-05-27 18:49:33 +0700

    update-app: update app to v2

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function App() {
 	const redundanceVar = '';
-	return <h1>Hello world! I am Phuoc Dinh Thien</h1>;
+	return <h1>Hello world! I am Phuoc Dinh Thien v2</h1>;
 }
 
 export default App;
--- app/src/tests/App.test.jsx
+++ app/src/tests/App.test.jsx
@@ -5,7 +5,7 @@ import App from '../App.jsx';
 describe('App tests', () => {
 	it('should contains the heading 1', () => {
 		render(<App />);
-		const heading = screen.getByText(/Hello world! I am Phuoc Dinh Thien/i);
+		const heading = screen.getByText(/Hello world! I am Phuoc Dinh Thien v2/i);
 		expect(heading).toBeInTheDocument();
 	});
 });
