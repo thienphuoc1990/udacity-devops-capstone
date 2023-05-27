8a4a259126a9ad614433d50f48492fb29e4e9f57
Phuoc Dinh Thien <dinhthienphuoc1990@gmail.com>
2023-05-27 17:55:42 +0700

    cicd-pipeline: correct smoke test

--- scripts/smoke-test.sh
+++ scripts/smoke-test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-http_code=$(curl -LI http://localhost:8080 -o /dev/null -w '%{http_code}\n' -s)
+http_code=$(curl -LI localhost:8080 -o /dev/null -w '%{http_code}\n' -s)
+echo $http_code
 if [[ ${http_code} -eq 200  ]]; then
     echo "Smoke test passed"
     exit 0
