df7ad72c4f415512510cc7748c97f0ac9bbbf977
Phuoc Dinh Thien <dinhthienphuoc1990@gmail.com>
2023-05-27 15:20:39 +0700

    initial-eks: add k8s local and prod

---
+++ scripts/deploy-network.sh
@@ -0,0 +1,14 @@
+
+#!/bin/bash
+ACTION=$1
+STACK_NAME=uda-devops-capstone-stack
+
+if [ $ACTION = "update" ]
+then
+    aws cloudformation update-stack \
+         --stack-name $STACK_NAME --template-body file://infra/network.yml --parameters file://infra/network-params.json
+elif [ $ACTION = "create" ]
+then
+    aws cloudformation create-stack \
+         --stack-name $STACK_NAME --template-body file://infra/network.yml --parameters file://infra/network-params.json
+fi
