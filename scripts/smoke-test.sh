612778990d7ec4851fbe4de584aa55643c3b2ee9
Phuoc Dinh Thien <dinhthienphuoc1990@gmail.com>
2023-05-27 18:44:17 +0700

    cicd-pipeline: extend waiting time

--- scripts/smoke-test.sh
+++ scripts/smoke-test.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo "Forwarding port to 8080 for run smoke test, wait 10 seconds for port forward"
+echo "Forwarding port to 8080 for run smoke test, wait 30 seconds for port forward"
+sleep 30
 kubectl port-forward service/uda-app-service 8080:80 > uda-app-forward-port.log 2>&1 &
 sleep 10
 http_code=$(curl -LI 127.0.0.1:8080 -o /dev/null -w '%{http_code}\n' -s)
